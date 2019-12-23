a69fc0e81fb54391cb1effb6a5bed2e31b8cfc18
Bengin <bengin@cetindere.de>
2019-12-23 19:37:46 +0100

    FileSyncManager method signatures

--- out/artifacts/Java_Socket_Client_jar/start.bat
+++
@@ -1,5 +0,0 @@
-java -jar Java_Socket_Client.jar
-java -jar Java_Socket_Client.jar
-java -jar Java_Socket_Client.jar
-java -jar Java_Socket_Client.jar
-java -jar Java_Socket_Client.jar
---
+++ src/files/FileSyncManager.java
@@ -0,0 +1,13 @@
+package files;
+
+import java.util.ArrayList;
+
+public class FileSyncManager {
+	public static void syncToFile() {
+		
+	}
+	
+	public static ArrayList<String> syncFromFile() {
+		return null;
+	}
+}
