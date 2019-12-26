327ac5c857e015d37013ee1ee7775289b03995f0
Bengin <bengin@cetindere.de>
2019-12-26 15:19:10 +0100

    FileSyncManager works for syncing ChatMessages to file, viceversa

--- src/files/FileSyncManager.java
+++ src/files/FileSyncManager.java
@@ -1,14 +1,21 @@
 package files;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import main.ChatMessage;
 
 public class FileSyncManager {
 	static BufferedWriter bw;
+	static BufferedReader br;
 
 
 	/**
@@ -16,7 +23,7 @@ public class FileSyncManager {
 	 * @param identifier a unique id for this instance (for example IP+Port)
 	 * @param messages the arraylist that contains the messages
 	 */
-	public static void syncToFile(String identifier, ArrayList<ChatMessage> messages) {
+	public static void syncToFile(String identifier, List<ChatMessage> messages) {
 		if (bw == null) {
 			try {
 				bw = new BufferedWriter(new FileWriter("./" + identifier + ".ciao"));
@@ -26,7 +33,15 @@ public class FileSyncManager {
 			}	
 		}
 		try {
-			bw.write("AC");
+			List<String> strings = new ArrayList<String>();
+			String messageString = "";
+			for(ChatMessage message : messages) {
+				messageString = 	message.getId() + ";"
+						+	message.getSender().replace(";", "\\;") + ";" 
+						+ 	message.getText().replace(";", "\\;") + "";
+				strings.add(messageString.replace("-", "\\-"));
+			}
+			bw.write(String.join("-", strings));
 			bw.flush();
 			bw.close();
 		} catch (IOException e) {
@@ -34,7 +49,32 @@ public class FileSyncManager {
 		}
 	}
 
-	public static ArrayList<String> syncFromFile() {
-		return null;
+	public static List<ChatMessage> syncFromFile(String identifier) {
+		if(br == null) {
+			try {
+				br = new BufferedReader(new FileReader(new File("./" + identifier + ".ciao")));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		List<ChatMessage> messages = new ArrayList<ChatMessage>();
+		try {
+			String fileIn = br.readLine();
+			List<String> messageStrings = Arrays.asList(fileIn.split("(?<!\\\\)-"));
+			
+			for(String messageString : messageStrings) {
+				messageString = messageString.replace("\\-", "-");
+				String[] messageMeta = messageString.split("(?<!\\\\);");
+				// escapes are in the messageMeta \; 
+				ChatMessage message = new ChatMessage(Integer.parseInt(messageMeta[0]), 
+						messageMeta[2].replace("\\;", ";"), 
+						messageMeta[1].replace("\\;", ";"));
+				messages.add(message);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return messages;
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,8 @@
 package main;
 
 import java.util.ArrayList;
-import main.ChatMessage;
+import java.util.List;
+
 import files.FileSyncManager;
 
 public class Main {
@@ -13,7 +14,16 @@ public class Main {
 //        listenerThread.start();
 //        senderThread.start();
         
-        FileSyncManager.syncToFile("192.168.178.51-2314", new ArrayList<ChatMessage>());
+//        List<ChatMessage> messages = new ArrayList<ChatMessage>();
+//        ChatMessage temp = new ChatMessage(3, "Lorem Ipsum xyz", "192.168.178.51-2314");
+//        messages.add(temp);
+//        temp = new ChatMessage(4, "Lorem Sips|um xyz", "192.168.178.51-2313");
+//        messages.add(temp);
+//        temp = new ChatMessage(4, "sldfj;sidjflskdf", "192.168.178.51-2314");
+//        messages.add(temp);
+//        messages = FileSyncManager.syncFromFile("192.168.178.51-2314");
+//        FileSyncManager.syncToFile("192.168.178.51-2314", messages);
+        
     }
 }
 
