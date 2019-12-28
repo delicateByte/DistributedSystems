ac4414508f7b5d565122baf8cbd7c7b55273e135
Jan <inf17021@lehre.dhbw-stuttgart.de>
2019-12-28 18:47:25 +0100

    adds new getter and Setter for Client

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,21 +1,23 @@
 package main;
+
 import java.net.*;
 import java.io.*;
 
-public class Client  {
-       private String ip;
-       private int port;
-       private String name;
-       private  int rights;
-       
-       public Client(String ip, int port) {
-    	   this.ip = ip;
-    	   this.port = port;
-       }
-	
-       public String getIp() {
-    	   return this.ip;
-       }
+public class Client {
+	private String ip;
+	private int port;
+	private String name;
+	private int rights;
+	private boolean active;
+
+	public Client(String ip, int port) {
+		this.ip = ip;
+		this.port = port;
+	}
+
+	public String getIp() {
+		return this.ip;
+	}
 
 	public int getPort() {
 		return port;
@@ -36,52 +38,52 @@ public class Client  {
 	public void setIp(String ip) {
 		this.ip = ip;
 	}
-       
-	/* private Socket clientSocket;
-        private PrintWriter out;
-        private BufferedReader in;
-        private Boolean connectionStatus =true;
 
-        @Override
-        public void run(){
-            startConnection("192.168.43.20",1337);
-            while(connectionStatus){
-                try{
-                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    System.out.println(in.readLine());
-                }catch (Exception e){
+	public int getRights() {
+		return rights;
+	}
 
-                }
-            }
-        }
-        public void startConnection(String ip, int port) {
-            try {
-                clientSocket = new Socket(ip, port);
-                System.out.println("test");
-                System.out.println(clientSocket);
+	public void setRights(int rights) {
+		this.rights = rights;
+	}
 
-            }catch(Exception e){
+	public boolean isActive() {
+		return active;
+	}
 
-            }
+	public void setActive(boolean active) {
+		this.active = active;
+	}
 
-        }
-
-        public String sendMessage(String msg) {
-            out.println(msg);
-
-            //String resp = in.readLine();
-            return "hi";
-        }
-
-        public void stopConnection() {
-            try{
-                in.close();
-                clientSocket.close();
-            } catch (Exception e){
-
-            }
-
-        }
-*/
+	/*
+	 * private Socket clientSocket; private PrintWriter out; private BufferedReader
+	 * in; private Boolean connectionStatus =true;
+	 * 
+	 * @Override public void run(){ startConnection("192.168.43.20",1337);
+	 * while(connectionStatus){ try{ in = new BufferedReader(new
+	 * InputStreamReader(clientSocket.getInputStream()));
+	 * System.out.println(in.readLine()); }catch (Exception e){
+	 * 
+	 * } } } public void startConnection(String ip, int port) { try { clientSocket =
+	 * new Socket(ip, port); System.out.println("test");
+	 * System.out.println(clientSocket);
+	 * 
+	 * }catch(Exception e){
+	 * 
+	 * }
+	 * 
+	 * }
+	 * 
+	 * public String sendMessage(String msg) { out.println(msg);
+	 * 
+	 * //String resp = in.readLine(); return "hi"; }
+	 * 
+	 * public void stopConnection() { try{ in.close(); clientSocket.close(); } catch
+	 * (Exception e){
+	 * 
+	 * }
+	 * 
+	 * }
+	 */
 
 }
