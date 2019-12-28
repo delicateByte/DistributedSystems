814510a9b3baec0921da8e58889303084d2b5bd2
Jan <inf17021@lehre.dhbw-stuttgart.de>
2019-12-28 21:04:48 +0100

    yep

--- src/networking/Phonebook.java
+++ src/networking/Phonebook.java
@@ -8,8 +8,7 @@ import main.Client;
 public class Phonebook {
 	// Contacts table
 	private ArrayList<Client> phonebook = new ArrayList<Client>();
-	
-	
+
 	public Client getLeader() {
 		Iterator<Client> itrClient = phonebook.iterator();
 		Client leader = null;
@@ -17,28 +16,40 @@ public class Phonebook {
 			if (itrClient.next().getRights() == 2) {
 				System.out.println(itrClient.next());
 				leader = itrClient.next();
-			}else {
-				//TODO: HOW HANDLE IF NO LEADER IS FOUND AT A POINT IN TIME 
+			} else {
+				// TODO: HOW HANDLE IF NO LEADER IS FOUND AT A POINT IN TIME
 			}
 		}
 		return leader;
-		
+
 	}
-	public void newLeader() {
-		
+
+	public void newLeader(Client clt) {
+		Iterator<Client> itrClient = phonebook.iterator();
+		while (itrClient.hasNext()) {
+			if (itrClient.next() == clt) {
+				itrClient.next().setRights(2);
+			} else {
+				// TODO: HOW HANDLE IF NO LEADER IS FOUND AT A POINT IN TIME
+			}
+		}
 	}
+
 	public void addNewNode() {
-		
+
 	}
-	public void deleteClient(Client client ) {
-		
+
+	public void deleteClient(Client client) {
+
 	}
+
 	public void deactivateClient() {
-		
+
 	}
+
 	public ArrayList<Client> getFullPhonebook() {
-		
+
 		return this.phonebook;
-		
+
 	}
 }
