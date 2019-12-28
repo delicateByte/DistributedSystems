781f15aca75707a32830b42aa81f8534239ef68a
Jan <inf17021@lehre.dhbw-stuttgart.de>
2019-12-28 22:53:29 +0100

    grober ablauf für init

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,6 +10,34 @@ public class Main {
     public static void main(String[] args) {
         Thread listenerThread = new Thread( new Listener());
         Thread senderThread = new Thread( new Sender());
+    	if(args[2]== "CONNECT") {
+    		/*
+    		One Joins a new Network
+    		- start Program with IP & Port of friend
+    		- I wanna join 
+    		- response Leader is this ask him OR Try again later there is none
+    		- Leader response none = wait and try again please 
+    		- LEader response Yes , take this Port and join me brother 
+    		- LEader sends a broadcast to all and says please add
+    		- Await response of all 
+    		- if no response from client xyz  retry with next heartbeat
+    		- if HEartbeat from LEader start Raft 
+    		- Here take the current phonebook
+    		- take current Messages
+    		- noraml raft stuff
+*/
+    		// Thread raftThread = new Thread(new Raft(senderThread));
+
+    	}else if(args[2]=="INIT") {
+    		  /*Create a Neyetwork
+    		- Port as arg   ( do you thing)  <---------------------------------------------------------- bengin
+    		- add myself to phonebook <------------------------------------------------------------------------- bengin 
+    		*/
+    		// Thread raftThread = new Thread(new Raft(senderThread));
+    	}
+		//  raftThread.start();
+
+
 
 //        listenerThread.start();
 //        senderThread.start();	
