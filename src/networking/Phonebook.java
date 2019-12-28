74c73f0c8ad18907ad1eeea97e4398161f73ee29
Jan <inf17021@lehre.dhbw-stuttgart.de>
2019-12-28 20:53:47 +0100

    Create Phonebook.java

---
+++ src/networking/Phonebook.java
@@ -0,0 +1,44 @@
+package networking;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import main.Client;
+
+public class Phonebook {
+	// Contacts table
+	private ArrayList<Client> phonebook = new ArrayList<Client>();
+	
+	
+	public Client getLeader() {
+		Iterator<Client> itrClient = phonebook.iterator();
+		Client leader = null;
+		while (itrClient.hasNext()) {
+			if (itrClient.next().getRights() == 2) {
+				System.out.println(itrClient.next());
+				leader = itrClient.next();
+			}else {
+				//TODO: HOW HANDLE IF NO LEADER IS FOUND AT A POINT IN TIME 
+			}
+		}
+		return leader;
+		
+	}
+	public void newLeader() {
+		
+	}
+	public void addNewNode() {
+		
+	}
+	public void deleteClient(Client client ) {
+		
+	}
+	public void deactivateClient() {
+		
+	}
+	public ArrayList<Client> getFullPhonebook() {
+		
+		return this.phonebook;
+		
+	}
+}
