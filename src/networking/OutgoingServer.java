130d13f41dd08bbfeca53243da07308775359db3
Jan <inf17021@lehre.dhbw-stuttgart.de>
2019-12-28 18:47:04 +0100

    changed recipient to Client for Type consitency

--- src/networking/OutgoingServer.java
+++ src/networking/OutgoingServer.java
@@ -1,5 +1,6 @@
 package networking;
 
+import main.Client;
 import main.Message;
 
 public class OutgoingServer {
@@ -7,12 +8,13 @@ public class OutgoingServer {
 	/**
 	 * Sends a network message to the recipient
 	 * @param message the Message object that should be sent
-	 * @param recipient the recipient in this form: 192.168.178.51-3538
+	 * @param client of Type Client that is disassembled into the recipient in this form: 192.168.178.51-3538
 	 * (ip + "-" + port)
 	 * @return returns if sending was error free (no confirmation if 
 	 * the message was delivered successfully). 
 	 */
-	public boolean sendMessage(Message message, String recipient) {
+	public boolean sendMessage(Message message, Client client) {
+		String recipient = client.getIp()+"-"+client.getPort();
 		return true;
 	}
 	
