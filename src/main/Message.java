570addbf07b5e25d24c33f6e4802fbbc0f8bde4d
Jan <inf17021@lehre.dhbw-stuttgart.de>
2019-12-29 01:13:34 +0100

    small task for you
    @xdivby0 , du bist mehr i Sender drinnen und wie der transmitted wird in welchem vormat und wie ich den zum Client Convertiere

--- src/main/Message.java
+++ src/main/Message.java
@@ -14,6 +14,9 @@ public class Message {
 	public String getSender() {
 		return sender;
 	}
+	public Client getSenderAsClient() {
+		return new Client("aas", 1);   //   <-------------------------bengin could you do that please
+	}
 	public String getPayload() {
 		return payload;
 	}
