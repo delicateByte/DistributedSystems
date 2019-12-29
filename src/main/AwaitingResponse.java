d4b0fda4c03bcbd9a66dd6d6b561bf5c38033c23
Jan <inf17021@lehre.dhbw-stuttgart.de>
2019-12-29 16:17:08 +0100

    adds Class for Tasklist

--- src/main/AwaitingResponse.java
+++ src/main/AwaitingResponse.java
@@ -1,5 +1,37 @@
 package main;
 
 public class AwaitingResponse {
+	private Client responder;
+	private MessageType type;
+	private String comparePayloads ;
+	
+	public AwaitingResponse(Client c, MessageType t) {
+		responder = c;
+		type = t;
+	}
 
+	public Client getResponder() {
+		return responder;
+	}
+
+	public void setResponder(Client responder) {
+		this.responder = responder;
+	}
+
+	public MessageType getType() {
+		return type;
+	}
+
+	public void setType(MessageType type) {
+		this.type = type;
+	}
+
+	public String getComparePayloads() {
+		return comparePayloads;
+	}
+
+	public void setComparePayloads(String comparePayloads) {
+		this.comparePayloads = comparePayloads;
+	}
+	
 }
