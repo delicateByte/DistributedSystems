283089d6b67607a10eb42e189485a407c024586f
Bengin <bengin@cetindere.de>
2019-12-29 01:46:31 +0100

    Message has also Client and not only senderString

--- src/main/Message.java
+++ src/main/Message.java
@@ -11,11 +11,17 @@ public class Message {
 		this.type = type;
 	}
 	
+	public Message(Client sender, String payload, MessageType type) {
+		this.sender = sender.getIp() + "-" + sender.getPort();
+		this.payload = payload;
+		this.type = type;
+	}
+	
 	public String getSender() {
 		return sender;
 	}
 	public Client getSenderAsClient() {
-		return new Client("aas", 1);   //   <-------------------------bengin could you do that please
+		return new Client(sender.split("-")[0], Integer.parseInt(sender.split("-")[1]));
 	}
 	public String getPayload() {
 		return payload;
